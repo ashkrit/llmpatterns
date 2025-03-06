3fde360e6f42b69a807acc6e36f88289be9eb4e8
ashkrit@gmail.com <ashkrit@gmail.com>
2025-03-06 12:29:22 +0800

    Basic Project

---
+++ src/main/java/org/llm/llmpatterns/App.java
@@ -0,0 +1,7 @@
+package org.llm.llmpatterns;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/llm/llmpatterns/AppTest.java
@@ -0,0 +1,7 @@
+package org.llm.llmpatterns;
+
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
