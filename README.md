40e0707e08d76fbdb7b332e370c67aace4dcec3a
ashkrit@gmail.com <ashkrit@gmail.com>
2025-03-06 16:34:55 +0800

    Update Blog Link

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # llmpatterns
 LLM Usage patterns
+
+
+# Blog Post on Patterns
+
+https://ashkrit.blogspot.com/2025/03/llm-patterns.html
+
--- src/test/java/org/llm/llmpatterns/AppTest.java
+++
@@ -1,7 +0,0 @@
-package org.llm.llmpatterns;
-
-public class AppTest {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
