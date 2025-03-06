8ceb9326bbd3473c3466a4031c52042e7cb39440
ashkrit@gmail.com <ashkrit@gmail.com>
2025-03-06 16:27:03 +0800

    Parameter Prompts example

---
+++ src/main/java/org/llm/llmpatterns/$005_ParameterChat.java
@@ -0,0 +1,64 @@
+package org.llm.llmpatterns;
+
+import com.google.gson.GsonBuilder;
+import org.llm.openai.GenerativeAIDriverManager;
+import org.llm.openai.impl.google.GoogleAIFactory;
+import org.llm.openai.model.ChatRequest;
+
+import java.util.List;
+import java.util.Map;
+
+public class $005_ParameterChat {
+    public static void main(String[] args) {
+        ServiceLoader.load();
+
+        Map<String, Object> properties = Map.of("apiKey", System.getenv("gemma_key"));
+        var service = GenerativeAIDriverManager.create(GoogleAIFactory.NAME, "https://generativelanguage.googleapis.com", properties);
+
+        var prompt = """
+                Top {{no_of_country}} Country by GPD. Reply in JSON format
+                Example:
+                {
+                "countries":[
+                    {"name":"country 1","gdp":gpd , "unit":"trillion or billion etc","rank":rank of country},
+                    {"name":"country 2","gdp":gpd , "unit":"trillion or billion etc","rank":rank of country},
+                    {"name":"country 3","gdp":gpd , "unit":"trillion or billion etc","rank":rank of country},
+                    {"name":"country 4","gdp":gpd , "unit":"trillion or billion etc","rank":rank of country},
+                    {"name":"country 5","gdp":gpd , "unit":"trillion or billion etc","rank":rank of country}
+                ]
+                }
+                """;
+        var messages = new ChatRequest.ChatMessage("user", prompt);
+        var conversation = ChatRequest.create("gemini-2.0-flash", List.of(messages));
+
+        var preparedConversion = service.prepareRequest(conversation, Map.of("no_of_country", "10"));
+        var reply = service.chat(preparedConversion, CountryGdp.class);
+
+        System.out.println(reply);
+    }
+
+
+    public static class CountryGdp {
+
+        public List<Country> countries;
+
+        public static class Country {
+            public String name;
+            public float gdp;
+            public String unit;
+            public int rank;
+
+            @Override
+            public String toString() {
+                var g = new GsonBuilder().setPrettyPrinting().create();
+                return g.toJson(this);
+            }
+        }
+
+        @Override
+        public String toString() {
+            var g = new GsonBuilder().setPrettyPrinting().create();
+            return g.toJson(this);
+        }
+    }
+}
